4d4c51ac36eb0792a6f87c3bdb39c1a0e79fa8ce
Aloys Baillet <aloys.baillet@gmail.com>
2019-03-27 22:56:43 +1100

    Cleanup and added debug info

--- conan/numpy/test_package/conanfile.py
+++ conan/numpy/test_package/conanfile.py
@@ -4,7 +4,6 @@ import os
 
 class NumpyTester(ConanFile):
     settings = "os", "compiler", "arch", "build_type"
-    requires = "numpy/1.12.1@aswf/vfx2018"
 
     def test(self):
         testfile = os.path.join(self.source_folder, 'test_numpy.py')
